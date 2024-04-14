4f550d30e6c1473a1205ceeedc680b23e1c3bbf6
douglas <douglas.noronha91@gmail.com>
2024-04-14 14:52:49 -0300

    update to zabbix6.4

--- install_run_ansible.sh
+++ install_run_ansible.sh
@@ -3,7 +3,6 @@
 echo "##############################################"
 echo "Verificar se o arquivo /etc/os-release existe!"
 echo "##############################################"
-
 if [ -e "/etc/os-release" ]; then
     ID=$(grep "^ID=" /etc/os-release | cut -d'=' -f2)
 else
@@ -26,15 +25,12 @@ echo "##########################################"
 echo "add private key for connect SSH with putty"
 echo "##########################################"
 printf "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCi/2BbT3vzuTo6FLd+YBafEQMVXebv42rZuaBaIMdeQhxUul76TD5QP0r2J7lOblqd5br2winXQUoe/Obnn6mHiWeqlPd6X9P9p4xUZWDTN0+SQ5HIGcHb4RjarvH7LJxkuEMco3l+Ey5fGnjV0k16+6dxFocDHkbXtePobt92xqJYTpjtPVNONPLzbfAQ0QYMnmZPOGuLF2GnJbn+nwZI3v2ko6dlcxdPPfj9bLcLqdsTevB1MS83GhwdEQevdJOKH6b/PETvw1MQ4I6zzOL4b7GwOZNLpiwT2+EgrKpSZjP4hdssjV8bv6083CO6qImcZvFFB2WHAmoUJJZT4Bql rsa-key-20240413" >> /home/vagrant/.ssh/authorized_keys
-      
+
 echo "#######################"
 echo "Rodar ansible-laybook!"
 echo "#######################"
-
 if [ "$(hostname)" == "zabbix-server" ]; then
-    # Verifica se o sistema é Debian
-    if [ $ID == debian ]; then
-        # Se ambas as condições forem verdadeiras, execute seu comando aqui
+        if [ $ID == debian ]; then        
         echo "########################"
         echo "Instalando zabbix-Server"
         echo "########################"
